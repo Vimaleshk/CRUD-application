e9511b01f004f2e551c6f1ca7a2a1af8e02e1039
Vimaleshk <149891581+Vimaleshk@users.noreply.github.com>
2026-03-08 10:26:15 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose")
 
 async function connectDB() {
-    await mongoose.connect("mongodb+srv://Vimalesh:Vky%40123@yt-backend.ldpox6o.mongodb.net/halley")
+    await mongoose.connect("mongodb+srv://Vimalesh:<db-password>@yt-backend.ldpox6o.mongodb.net/halley")
 
     console.log("Database Connected")
 }
+
 
 module.exports = connectDB
